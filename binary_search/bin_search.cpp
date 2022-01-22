0918a50dd94d59b90df67ab464ee73180b30ee8f
zlatatikhomirova <72922674+zlatatikhomirova@users.noreply.github.com>
2022-01-22 13:15:47 +0700

    Create bin_search.cpp

---
+++ binary_search/bin_search.cpp
@@ -0,0 +1,46 @@
+int bin_search(int x, L, R) 
+    { 
+        int left, right, m; 
+        left = L; right = R; 
+        while (right - left > 1) 
+        { 
+            m = (right + left) / 2; 
+            if (a[m] < x) left = m; 
+            else right = m; 
+        } 
+    return right; 
+    }
+while (l<r) {
+m=(l+r)/2;
+if (a[m]<k) l=m+1;
+else r=m;
+}
+if (a[r]==k) printf("%d", r);
+else printf("-1");
+
+r = x;
+l = 1;
+while (fabs(l-r)>eps) {
+m=(l+r)/2;
+if (m*m*m<x) l=m;
+else r=m;
+}
+
+int main(void)
+{
+int n, x, y, i, j, l, r, now;
+double speed;
+scanf("%d%d%d", &n, &i, &j);
+x=i<j?i:j;
+y=i>j?i:j;
+l=0;
+r = (n-1)*y;
+while (l != r) {
+now = (l+r)/2;
+j = now / x + now / y;
+if (j < n-1) l = now+1;
+else r = now;
+}
+printf("%d", r+x);
+return 0;
+}
